7a27e2afebadf11e74aece2cfa0df242d1a6446f
fabio <fabio.info@gmail.com>
2014-01-08 20:21:19 -0200

    posts reviwed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.published
     @page_title = t("main.home_title")
+    @years_coding = (Date.today.year - 2001)
   end
 
   def show
