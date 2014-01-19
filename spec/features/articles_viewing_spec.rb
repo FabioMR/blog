a35ae04cac13227d97c74f05f3a5742ee01d6e80
fabio <fabio.info@gmail.com>
2014-01-19 15:56:13 -0200

    features test

---
+++ spec/features/articles_viewing_spec.rb
@@ -0,0 +1,45 @@
+require "spec_helper"
+
+describe "Articles viewing" do
+
+  let!(:article_1) { Article.create!(title: "TITLE-1", description: "DESCRIPTION-1", slug: "SLUG-1", published_at: Date.yesterday) }
+  let!(:article_2) { Article.create!(title: "TITLE-2", description: "DESCRIPTION-2", slug: "SLUG-2", published_at: Date.today) }
+
+  context "User visits articles list" do
+    before do
+      visit root_path
+    end
+
+    it "has 2 articles" do
+      expect(page.all(".article")).to have(2).itens
+    end
+  end
+
+  context "User visits article" do
+    before do
+      visit root_path
+      click_link article_1.title
+    end
+
+    it "redirect to article path" do
+      expect(current_path).to eql(article_path(article_1.slug))
+    end
+
+    it "shows article description" do
+      expect(page).to have_text(article_1.description)
+    end
+  end
+
+  context "User comes back to articles list" do
+    before do
+      visit root_path
+      click_link article_1.title
+      click_link t("main.header.title")
+    end
+
+    it "redirect to article path" do
+      expect(current_path).to eql(root_path)
+    end
+  end
+
+end
