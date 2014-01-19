d7b447a7bd16772d3be32ab3a5718596c9502ee7
fabio <fabio.info@gmail.com>
2014-01-19 12:54:08 -0200

    services test adjustment

--- spec/services/years_of_coding_spec.rb
+++ spec/services/years_of_coding_spec.rb
@@ -1,11 +1,17 @@
-# require "./app/services/years_of_coding"
 require "spec_helper"
 
 describe YearsOfCoding do
 
-  it ".till_now" do
-    time_travel_to Date.new(2014, 1, 1)
-    expect(YearsOfCoding.till_now).to eql(13)
+  context ".till_now" do
+    it "returns 13 in 2014" do
+      time_travel_to Date.new(2014, 1, 1)
+      expect(YearsOfCoding.till_now).to eql(13)
+    end
+
+    it "returns 14 in 2015" do
+      time_travel_to Date.new(2015, 1, 1)
+      expect(YearsOfCoding.till_now).to eql(14)
+    end
   end
 
 end
